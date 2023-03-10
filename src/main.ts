114a20c647e21581feb09c01ab20e84c44bbb728
fangjiebiao <fangjiebiao@migu.cn>
2023-03-10 17:47:17 +0800

    chore: 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
+import router from './router'
+import store from './store'
+import directives from './directives'
 import App from './App.vue'
 import './assets/style/wind.css'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
+import zhCN from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(directives)
+    .use(router)
+    .use(store)
+    .use(ElementPlus, { locale: zhCN })
+    .mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import { ConfigEnv, UserConfigExport, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+function createTime () {
+    const now = new Date()
+	const year = now.getFullYear()
+    const month = `0${now.getMonth() + 1}`.padStart(2, '0')
+    const date = `0${now.getDate()}`.padStart(2, '0')
+    const hour = `0${now.getHours()}`.padStart(2, '0')
+    const minutes = `0${now.getMinutes()}`.padStart(2, '0')
+	const seconds = `0${now.getSeconds()}`.padStart(2, '0')
+    return `${year}-${month}-${date} ${hour}:${minutes}:${seconds}`
+}
+
+
+export default (config: ConfigEnv): UserConfigExport => {
+    const { mode, command } = config
+    // dev mode: development command: serve
+    console.log(config)
+    
+    const userConfig: UserConfigExport = {
+        root: process.cwd(),
+		base: './',
+        resolve: {
+			alias: [{
+                find: '@',
+                replacement: resolve(__dirname, './src')
+			}],
+			extensions: ['.js', '.ts', '.json', '.vue']
+		},
+        plugins: [
+            vue()
+        ]
+    }
+    return userConfig
+}
